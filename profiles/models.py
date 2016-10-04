81e252a45efce055d6f9b2e671ade488dcf22f62
ubuntu <jerinzam@gmail.com>
2016-10-04 20:44:25 +0530

    First commit

---
+++ profiles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    mob_no = models.CharField(max_length=12)
+    
+    def __unicode__(self):
+	return self.user.username
